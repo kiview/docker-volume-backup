186a55fac4462da4a1a58e62fd782509200a3680
Kevin Wittek <kevin.wittek@gdata-adan.de>
2016-10-19 13:26:02 +0200

    :tada: Add backup script

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # docker-volume-backup
 Scripts for easy backup and restore of Docker volumes
+
+## Usage
+
+```
+./docker_volume_backup.sh {compose_file_path} {project_name} {backup_path} {backup_or_restore} {restore_date}
+```
+
+## Examples
+
+Backup
+
+```
+./docker_volume_backup.sh /home/kiview/Gitlab/docker-compose.yml gitlab $(pwd)/backup backup
+```
+
+Restore
+
+```
+./docker_volume_backup.sh /home/kiview/Gitlab/docker-compose.yml gitlab $(pwd)/backup restore 2016-10-19
+```
---
+++ docker_volume_backup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+compose_file_path=$1
+project_name=$2
+backup_path=$3
+backup_or_restore=$4
+restore_date=$5
+
+set -e
+
+function backup_volume {
+  volume_name=$1
+  backup_destination=$2
+  date_suffix=$(date -I)
+
+  docker run --rm -v $volume_name:/data -v $backup_destination:/backup ubuntu tar -zcvf /backup/$volume_name-$date_suffix.tar /data
+}
+
+function restore_volume {
+  volume_name=$1
+  backup_destination=$2
+  date=$3
+
+  docker run --rm -v $volume_name:/data ubuntu find /data -mindepth 1 -delete
+  docker run --rm -v $volume_name:/data -v $backup_destination:/backup ubuntu tar -xvf /backup/$volume_name-$date.tar -C .
+}
+
+function main {
+  echo "Stopping running containers"
+  docker-compose -f $compose_file_path -p $project_name stop
+
+  echo "Mounting volumes and performing backup/restore..."
+  volumes=($(docker volume ls -f name=$project_name | awk '{if (NR > 1) print $2}'))
+  for v in "${volumes[@]}"
+  do
+    if [ "$backup_or_restore" == "backup" ]
+    then
+      echo "Perform backup"
+      backup_volume $v $backup_path
+    fi
+
+    if [ "$backup_or_restore" == "restore" ]
+    then
+      echo "Restore from backup"
+      restore_volume $v $backup_path $restore_date
+    fi
+  done
+
+  echo "Restarting containers"
+  docker-compose -f $compose_file_path -p $project_name start
+}
+
+main
